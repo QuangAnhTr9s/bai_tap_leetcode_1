a20ca9ce45e6fb8a2c7a5c711d5a3a031d54eee8
QuangAnhTr9s <trinhquanganh210799@gmail.com>
2022-12-15 18:48:36 +0700

    xóa điều kiện i>3, i>5

--- bin/bai_2.dart
+++ bin/bai_2.dart
@@ -1,13 +1,13 @@
 //412. Fizz Buzz
 void main(){
-  print(fizzBuzz(15));
+  print(fizzBuzz(30));
 }
 List<String> fizzBuzz(int n) {
   List<String> list = [];
   for(int i = 1; i <= n; i++){
-    if((i>= 5 && i%3 == 0) && i%5 == 0){list.add('FizzBuzz');}
-    else if(i>= 3 && i%3 == 0){list.add('Fizz');}
-    else if(i>=5 && i%5 == 0){list.add('Buzz');}
+    if((i%3 == 0) && i%5 == 0){list.add('FizzBuzz');}
+    else if(i%3 == 0){list.add('Fizz');}
+    else if(i%5 == 0){list.add('Buzz');}
     else {list.add(i.toString());};
   }
     return list;
